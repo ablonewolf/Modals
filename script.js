2f7b871f08b90c80dca713072842d968b270148e
ab <arkabhuiyan66@gmail.com>
2022-01-16 18:24:32 +0600

    Adding button functionalities

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-'use strict';
+"use strict";
+const Modals = document.querySelector(".modal");
+const overlays = document.querySelector(".overlay");
+const OpenButtonModals = document.querySelectorAll(".show-modal");
+const CloseButtonModals = document.querySelectorAll(".close-modal");
+
+const CloseModals = function () {
+  Modals.classList.add("hidden");
+  overlays.classList.add("hidden");
+};
+
+const OpenModals = function () {
+  Modals.classList.remove("hidden");
+  overlays.classList.remove("hidden");
+};
+// console.log(Modals.textContent);
+for (let i = 0; i < OpenButtonModals.length; i++) {
+  //   console.log(OpenButtonModals[i].textContent);
+  OpenButtonModals[i].addEventListener("click", OpenModals);
+}
+
+CloseButtonModals.addEventListener("click", CloseModals);
+overlays.addEventListener("click", CloseModals);
